0e3f51f81e79c63360bb49ae5bb5a7e1c35f602b
Fishbite <43952420+Fishbite@users.noreply.github.com>
2024-07-15 15:06:07 +0100

    mailto link: added subject and body text

--- footer.php
+++ footer.php
@@ -3,7 +3,8 @@
 echo '
     <footer>
       <p class="footer">Copyright &copy; fishbite 2024</p>
-      <a href="mailto:email: fishbitestu@gmail.com">fishbitestu@gmail.com</a>
+      <a href="mailto:fishbitestu@gmail.com?subject=I need a website &body=Hey Fishbite, would you reach out to me regarding my website needs...
+      %0D%0AI\'m not sure where to start...%0D%0AThanks">fishbitestu@gmail.com</a>
     </footer>
     <script>/*Some JS comment*/</script>
   </body>
